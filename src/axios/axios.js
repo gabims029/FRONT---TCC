bb29fea78c757b47aacd1e07d2cadef0a056d71a
anaesparapani <anaclaraesparapani@gmail.com>
2025-11-12 08:25:42 -0300

    Implantado

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.89.240.117:3000/api",
+  baseURL: "http:http://tcc.southafricanorth.cloudapp.azure.com/",
   headers: { accept: "application/json" },
 });
 
